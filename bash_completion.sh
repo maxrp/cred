dac3b6ab3725b33d4980147cd130d093cd64ef24
Max Parmer <m@x-pl.us>
2011-12-11 23:36:17 -0800

    preliminary completion support for `cred {add,get,schema} (*)`

---
+++ bash_completion.sh
@@ -0,0 +1,25 @@
+_cred()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # Cred's verbs
+    opts="add get schema"
+
+    # generate the appropriate completions, right now just for `cred get`
+    case "${prev}" in
+        get)
+            local credentials=$(cred get)
+            COMPREPLY=( $(compgen -W "${credentials}" -- ${cur}) )
+            return 0
+            ;;
+        *)
+        ;;
+    esac
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _cred cred
