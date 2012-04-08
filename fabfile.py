9dbe91f2396292c1d076bd05955c31269d7e7062
Max Parmer <m@x-pl.us>
2012-04-08 00:00:38 -0700

    decluttering task

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,9 @@
 from fabric.api import local
 
+def clean():
+    local('find . -name "*.pyc" -exec rm -rf {} \;')
+    local('rm -rf cred.egg-info dist build cred.egg-info pylint.log')
+
 def lint():
     local('pylint cred/*.py setup.py | tee pylint.log | less')
 
