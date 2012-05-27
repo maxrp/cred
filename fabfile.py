a9850a2684bca3e834c1f07f9f9847c77c4bb3f6
Max Parmer <m@x-pl.us>
2012-05-26 20:18:42 -0700

    gpg test fixtures

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,21 @@
 from fabric.api import local
+from os import path
+
+def make_gpg_fixtures(fixtures='./tests/fixtures'):
+    secring_exists = path.exists('{}/gpghome/secring.gpg'.format(fixtures))
+    pubring_exists = path.exists('{}/gpghome/pubring.gpg'.format(fixtures))
+    if not secring_exists and not pubring_exists:
+        local('gpg --homedir={0} --batch \
+              --gen-key {0}/gpg-gen-key.params'.format(fixtures))
+    else:
+        print 'Skipping key generation, keyrings exist.'
 
 def clean():
     local('find . -name "*.pyc" -exec rm -rf {} \;')
     local('rm -rf cred.egg-info dist build cred.egg-info pylint.log test.log')
+
+def gpg_clean(fixtures='./tests/fixtures'):
+    local('rm -rf {0}/gpghome/*'.format(fixtures))
 
 def lint():
     local('pylint cred/*.py setup.py tests/*.py | tee pylint.log | less')
